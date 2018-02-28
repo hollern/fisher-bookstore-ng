62f8c00250cbc9b891206c014a3c87e16c768d4c
Conor Hollern <Conor@Conors-MacBook-Pro.local>
2018-02-28 18:39:44 -0500

    Added routing and API access

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
 import { BooksComponent } from './books/books.component';
+import { BookDetailComponent } from './book-detail/book-detail.component';
+import { OrdersComponent } from './orders/orders.component';
+import { HomeComponent } from './home/home.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { FooterComponent } from './footer/footer.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { AppRouterModule } from './/router.module';
+import { BooksService } from './books/books.service';
 
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BooksComponent
+    BooksComponent,
+    BookDetailComponent,
+    OrdersComponent,
+    HomeComponent,
+    NavBarComponent,
+    FooterComponent,
+    NotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRouterModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [BooksService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -1,17 +1,31 @@
 import { Component, OnInit } from '@angular/core';
-import { Book } from '../books/book';
-import { BOOKS } from '../books/mock-book-service';
+
+import { IBook } from '../books/book';
+import { BooksService } from '../books/books.service';
 
 @Component({
   selector: 'app-books',
   templateUrl: './books.component.html',
   styleUrls: ['./books.component.css']
 })
+
 export class BooksComponent implements OnInit {
-  
-  books = BOOKS;
+  books: IBook[] = [];
+  selectedBook: IBook;
+
+  constructor(private _booksService: BooksService) { }
 
   ngOnInit() {
+    this._booksService.getBooks()
+      .subscribe(books => {
+        this.books = books;
+      },
+        error => console.log(error)
+      );
+  }
+
+  onSelect(book: IBook): void {
+    this.selectedBook = book;
   }
 
 }
